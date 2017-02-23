54e06bb0f13610a12a5de51ad0f2250fb9d428b4
Reece Volker <Reece Volker>
2017-02-23 20:14:11 +0200

    first intial

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+int noVideos;
+int noEndpoints;
+int noRequestDescriptions;
+int noCaches;
+int cacheSize;
+
+vector<int> videoSizes;
+
+
+void getInput()
+{
+
+}
+int main()
+{
+
+}
